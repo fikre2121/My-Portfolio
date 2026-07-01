15be76d304cc39eb6d87f5be1e67f46564934afd
Fikre2121 <fikre.gebremariam2121@gmail.com>
2026-07-01 18:20:12 +0300

    the images are changed

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,5 @@
-import p1 from "../assets/project-1.jpg";
-import p2 from "../assets/project-2.jpg";
+import p1 from "../assets/project-1.png";
+import p2 from "../assets/project-2.png";
 import p3 from "../assets/project-3.jpg";
 import p4 from "../assets/project-4.jpg";
 
